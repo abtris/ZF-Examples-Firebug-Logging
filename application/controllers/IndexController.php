3efc2762e468c11cb01153d533ff48fa664ccf48
Ladislav Prskavec <ladislav.prskavec@gmail.com>
2010-03-04 16:23:49 +0100

    Add log to Firebug

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -10,7 +10,16 @@ class IndexController extends Zend_Controller_Action
 
     public function indexAction()
     {
-        // action body
+        // writer
+        $writer = new Zend_Log_Writer_Firebug();
+        // log
+        $log = new Zend_Log($writer);
+
+        // messages
+        $log->log($_REQUEST, Zend_Log::ERR);
+        $log->log($_ENV, Zend_Log::ALERT);        
+        $log->log($_SERVER, Zend_Log::ALERT);
+
     }
 
 
